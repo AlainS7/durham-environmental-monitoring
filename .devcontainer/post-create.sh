f7865d62bd7b6c295d6dd3c2fa2ad93501fe7103
Alain Soto <134567742+AlainS7@users.noreply.github.com>
2025-10-08 17:23:03 +0000

    fix/post-create gcloud sdk

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,10 +7,12 @@ echo "Starting post-create setup..."
 echo "Installing Google Cloud CLI..."
 sudo apt-get update && sudo apt-get install -y lsb-release apt-transport-https ca-certificates gnupg
 
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install -y google-cloud-cli
 
+sudo mkdir -p /usr/share/keyrings
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
 sudo apt-get update && sudo apt-get install -y google-cloud-cli
 
 # Create and activate a virtual environment
