81cfb2b3c938143067af39a1b67450e115a393fb
Alain Soto <134567742+AlainS7@users.noreply.github.com>
2025-10-14 22:35:49 +0000

    fix: Simplify where clause in dbt model to avoid parser error

--- transformations/dbt/models/marts/sensor_readings_long.sql
+++ transformations/dbt/models/marts/sensor_readings_long.sql
@@ -14,8 +14,7 @@ with wu_src as (
       solar_radiation,
       uv_high
     from {{ ref('stg_wu_raw') }}
-    where ts is not null
-      and date(ts) between date_sub(var('proc_date'), interval 0 day) and var('proc_date')
+    where date(ts) = var('proc_date')
 ), tsi_src as (
     select
       ts as timestamp,
@@ -24,8 +23,7 @@ with wu_src as (
       humidity,
       temperature
     from {{ ref('stg_tsi_raw') }}
-    where ts is not null
-      and date(ts) between date_sub(var('proc_date'), interval 0 day) and var('proc_date')
+    where date(ts) = var('proc_date')
 ), wu_long as (
     select * from wu_src
     unpivot (value for metric_name in (temperature, humidity, precip_rate, precip_total, wind_speed_avg, wind_gust_avg, wind_direction_avg, solar_radiation, uv_high))
